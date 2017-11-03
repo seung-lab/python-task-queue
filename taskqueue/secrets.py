61780be66148fc2b051b1582072b7554985e52c7
William Silversmith <william.silversmith@gmail.com>
2017-11-03 17:35:00 -0400

    refactor: use cloudvolume.secrets instead of reimplementing it

--- taskqueue/secrets.py
+++ taskqueue/secrets.py
@@ -5,55 +5,17 @@ import json
 
 from google.oauth2 import service_account
 from cloudvolume.lib import mkdir, colorize
+from cloudvolume.secrets import (
+  CLOUD_VOLUME_DIR, PROJECT_NAME, 
+  google_credentials_path, google_credentials,
+  aws_credentials, aws_credentials_path, 
+  boss_credentials, boss_credentials_path
+)
 
 QUEUE_NAME = '' if 'PIPELINE_USER_QUEUE' not in os.environ else os.environ['PIPELINE_USER_QUEUE']
 TEST_QUEUE_NAME = '' if 'TEST_PIPELINE_USER_QUEUE' not in os.environ else os.environ['TEST_PIPELINE_USER_QUEUE']
 QUEUE_TYPE = 'pull-queue' if 'QUEUE_TYPE' not in os.environ else os.environ['QUEUE_TYPE']
-PROJECT_NAME = 'neuromancer-seung-import' 
 APPENGINE_QUEUE_URL = 'https://queue-dot-neuromancer-seung-import.appspot.com'
 
-CLOUD_VOLUME_DIR = mkdir(os.path.join(os.environ['HOME'], '.cloudvolume'))
-
-def secretpath(filepath):
-  preferred = os.path.join(CLOUD_VOLUME_DIR, filepath)
-  
-  if os.path.exists(preferred):
-    return preferred
-
-  backcompat = [
-    os.path.join(os.environ['HOME'], '.neuroglancer'), # older
-    '/' # original
-  ]
-
-  backcompat = [ os.path.join(path, filepath) for path in backcompat ] 
-
-  for path in backcompat:
-    if os.path.exists(path):
-      print(colorize('yellow', 'Deprecation Warning: {} is now preferred to {}.'.format(preferred, path)))  
-      return path
-
-  return preferred
-
-PROJECT_NAME = None 
-project_name_paths = [ 
-  secretpath('secrets/project_name'),
-  secretpath('project_name')
-]
-
-if 'GOOGLE_PROJECT_NAME' in os.environ: 
-  PROJECT_NAME = os.environ['GOOGLE_PROJECT_NAME']
-else: 
-  for path in project_name_paths:
-    if os.path.exists(path):
-      with open(path, 'r') as f:
-        PROJECT_NAME = f.read().strip()
-      break
-
-google_credentials_path = secretpath('secrets/google-secret.json')
-if os.path.exists(google_credentials_path):
-  google_credentials = service_account.Credentials \
-    .from_service_account_file(google_credentials_path)
-else:
-  google_credentials = ''
 
 
