c419fbde796ceedeabeb387d8365b45b6c689637
William Silversmith <william.silversmith@gmail.com>
2018-10-12 10:53:40 -0400

    chore: change tab width to 2

--- taskqueue/appengine_queue_api.py
+++ taskqueue/appengine_queue_api.py
@@ -6,86 +6,86 @@ import requests
 from .secrets import APPENGINE_QUEUE_URL, PROJECT_NAME, QUEUE_NAME
 
 class AppEngineTaskQueueAPI(object):
-    def __init__(self, project=PROJECT_NAME, queue_name=QUEUE_NAME):
-        self._project = project 
-        self._queue_name = queue_name
+  def __init__(self, project=PROJECT_NAME, queue_name=QUEUE_NAME):
+    self._project = project 
+    self._queue_name = queue_name
 
-    def base_url(self):
-        return join(APPENGINE_QUEUE_URL, '{PROJECT_ID}/taskqueue/{QUEUE_ID}/').format(
-            PROJECT_ID=self._project,
-            QUEUE_ID=self._queue_name,
-        )
+  def base_url(self):
+    return join(APPENGINE_QUEUE_URL, '{PROJECT_ID}/taskqueue/{QUEUE_ID}/').format(
+      PROJECT_ID=self._project,
+      QUEUE_ID=self._queue_name,
+    )
 
-    @property
-    def enqueued(self):
-        tqinfo = self.status()
-        return int(tqinfo['stats']['totalTasks'])
+  @property
+  def enqueued(self):
+    tqinfo = self.status()
+    return int(tqinfo['stats']['totalTasks'])
 
-    def status(self):
-        return requests.get(self.base_url()).json()
+  def status(self):
+    return requests.get(self.base_url()).json()
 
-    def insert(self, body):
-        if 'groupByTag' not in body or not body['groupByTag']:
-            body['tag'] = ''
-        
-        if 'groupByTag' in body: del body['groupByTag']
-        if 'queueName' in body: del body['queueName']
+  def insert(self, body):
+    if 'groupByTag' not in body or not body['groupByTag']:
+      body['tag'] = ''
+    
+    if 'groupByTag' in body: del body['groupByTag']
+    if 'queueName' in body: del body['queueName']
 
-        url = join(self.base_url(), 'tasks')
-        return requests.post(url, data=json.dumps(body)).json()
+    url = join(self.base_url(), 'tasks')
+    return requests.post(url, data=json.dumps(body)).json()
 
-    def get(self, tid):
-        endpoint = 'tasks/id/{TASK_ID}'.format(TASK_ID=tid)
-        url = join(self.base_url(), endpoint)
-        return requests.get(url).json()
+  def get(self, tid):
+    endpoint = 'tasks/id/{TASK_ID}'.format(TASK_ID=tid)
+    url = join(self.base_url(), endpoint)
+    return requests.get(url).json()
 
-    def acknowledge(self, tid):
-        return requests.delete(tid).json()
+  def acknowledge(self, tid):
+    return requests.delete(tid).json()
 
-    def delete(self, tid):
-        endpoint = 'tasks/id/{TASK_ID}'.format(TASK_ID=tid)
-        url = join(self.base_url(), endpoint)
-        requests.delete(url)
+  def delete(self, tid):
+    endpoint = 'tasks/id/{TASK_ID}'.format(TASK_ID=tid)
+    url = join(self.base_url(), endpoint)
+    requests.delete(url)
 
-    def purge(self):
-        url = join(self.base_url(), 'purge')
-        requests.post(url)
+  def purge(self):
+    url = join(self.base_url(), 'purge')
+    requests.post(url)
 
-    def renew_lease(self, task_id, seconds):
-        raise NotImplementedError()
-        # endpoint = 'tasks/{TASK_ID}:renewLease'.format(TASK_ID=tid)
-        # url = join(self.base_url(), endpoint)
-        # return post(url, data=json.dumps({ 'leaseSecs': seconds }))
+  def renew_lease(self, task_id, seconds):
+    raise NotImplementedError()
+    # endpoint = 'tasks/{TASK_ID}:renewLease'.format(TASK_ID=tid)
+    # url = join(self.base_url(), endpoint)
+    # return post(url, data=json.dumps({ 'leaseSecs': seconds }))
 
-    def cancel_lease(self, task_id):
-        raise NotImplementedError()
-        # endpoint = 'tasks/{TASK_ID}:cancelLease'.format(TASK_ID=tid)
-        # url = join(self.base_url(), endpoint)
-        # return post(url)
+  def cancel_lease(self, task_id):
+    raise NotImplementedError()
+    # endpoint = 'tasks/{TASK_ID}:cancelLease'.format(TASK_ID=tid)
+    # url = join(self.base_url(), endpoint)
+    # return post(url)
 
-    def lease(self, numTasks=1, leaseSecs=1, groupByTag=False, tag=''):
-        if groupByTag == False:
-            tag = ''
-        
-        params = {
-            'numTasks': numTasks, 
-            'leaseSecs': leaseSecs, 
-            'tag': tag,
-        }
+  def lease(self, numTasks=1, leaseSecs=1, groupByTag=False, tag=''):
+    if groupByTag == False:
+      tag = ''
+    
+    params = {
+      'numTasks': numTasks, 
+      'leaseSecs': leaseSecs, 
+      'tag': tag,
+    }
 
-        url = join(self.base_url(), 'tasks/lease')
-        return requests.post(url, data=json.dumps(params)).json()
+    url = join(self.base_url(), 'tasks/lease')
+    return requests.post(url, data=json.dumps(params)).json()
 
-    def list(self):
-        url = join(self.base_url(), 'tasks')
-        return requests.get(url).json()
+  def list(self):
+    url = join(self.base_url(), 'tasks')
+    return requests.get(url).json()
 
 
 class ExecuteWrapper(object):
-    def __init__(self, result):
-        self.result = result
-    def execute(self, num_retries=6):
-        if type(self.result) is list and len(self.result):
-            # mimicking  Google's Pull Queue API
-            return { 'items': self.result } 
-        return self.result
+  def __init__(self, result):
+    self.result = result
+  def execute(self, num_retries=6):
+    if type(self.result) is list and len(self.result):
+      # mimicking  Google's Pull Queue API
+      return { 'items': self.result } 
+    return self.result
--- taskqueue/registered_task.py
+++ taskqueue/registered_task.py
@@ -13,82 +13,82 @@ import numpy as np
 registry = {}
 
 def register_class(target_class):
-    registry[target_class.__name__] = target_class
+  registry[target_class.__name__] = target_class
 
 def deserialize(data):
-    params = json.loads(data)
-    name = params['class']
-    target_class = registry[name]
-    del params['class']
-    return target_class(**params)
+  params = json.loads(data)
+  name = params['class']
+  target_class = registry[name]
+  del params['class']
+  return target_class(**params)
 
 def payloadBase64Decode(payload):
-    decoded_string = base64.b64decode(payload).decode('utf8')
-    return deserialize(decoded_string)
+  decoded_string = base64.b64decode(payload).decode('utf8')
+  return deserialize(decoded_string)
 
 class Meta(type):
-    def __new__(meta, name, bases, class_dict):
-        cls = type.__new__(meta, name, bases, class_dict)
-        register_class(cls)
-        cls._arg_names = inspect.getargspec(class_dict['__init__'])[0][1:]
-        return cls
+  def __new__(meta, name, bases, class_dict):
+    cls = type.__new__(meta, name, bases, class_dict)
+    register_class(cls)
+    cls._arg_names = inspect.getargspec(class_dict['__init__'])[0][1:]
+    return cls
 
 class RegisteredTask(with_metaclass(Meta)):
-    def __init__(self, *arg_values, **kwarg_values):
-        self._args = OrderedDict(zip(self._arg_names, arg_values))
-        self._args.update(kwarg_values)
+  def __init__(self, *arg_values, **kwarg_values):
+    self._args = OrderedDict(zip(self._arg_names, arg_values))
+    self._args.update(kwarg_values)
 
-    @classmethod
-    def deserialize(cls, base64data):
-        obj = deserialize(base64data)
-        assert type(obj) == cls
-        return obj
-        
-    def serialize(self):
-        d = copy.deepcopy(self._args)
-        d['class'] = self.__class__.__name__
+  @classmethod
+  def deserialize(cls, base64data):
+    obj = deserialize(base64data)
+    assert type(obj) == cls
+    return obj
+    
+  def serialize(self):
+    d = copy.deepcopy(self._args)
+    d['class'] = self.__class__.__name__
 
-        def serializenumpy(obj):
-            if isinstance(obj, np.ndarray):
-                return obj.tolist()
-            return obj
+    def serializenumpy(obj):
+      if isinstance(obj, np.ndarray):
+        return obj.tolist()
+      return obj
 
-        return json.dumps(d, default=serializenumpy)
+    return json.dumps(d, default=serializenumpy)
 
-    @property
-    def payloadBase64(self):
-        return base64.b64encode(self.serialize().encode('utf8'))
+  @property
+  def payloadBase64(self):
+    return base64.b64encode(self.serialize().encode('utf8'))
 
-    @property
-    def id(self):
-        return self._id
+  @property
+  def id(self):
+    return self._id
 
-    def __repr__(self):
-        string = self.__class__.__name__ + "("
-        for arg_name, arg_value in six.iteritems(self._args):
-            if isinstance(arg_value, six.string_types):
-                string += "{}='{}',".format(arg_name, arg_value)
-            else:
-                string += "{}={},".format(arg_name, arg_value)
+  def __repr__(self):
+    string = self.__class__.__name__ + "("
+    for arg_name, arg_value in six.iteritems(self._args):
+      if isinstance(arg_value, six.string_types):
+        string += "{}='{}',".format(arg_name, arg_value)
+      else:
+        string += "{}={},".format(arg_name, arg_value)
 
-        # remove the last comma if necessary
-        if string[-1] == ',':
-            string = string[:-1]
+    # remove the last comma if necessary
+    if string[-1] == ',':
+      string = string[:-1]
 
-        return string + ")"  
+    return string + ")"  
 
 class PrintTask(RegisteredTask):
-    def __init__(self, txt=''):
-        super(PrintTask, self).__init__()
-        self.txt = txt
-    def execute(self):
-        if self.txt:
-            print(str(self) + ": " + str(self.txt))
-        else:
-            print(self)
+  def __init__(self, txt=''):
+    super(PrintTask, self).__init__()
+    self.txt = txt
+  def execute(self):
+    if self.txt:
+      print(str(self) + ": " + str(self.txt))
+    else:
+      print(self)
 
 class MockTask(RegisteredTask):
-    def __init__(self, **kwargs):
-        super(MockTask, self).__init__(**kwargs)
-    def execute(self):
-        pass
+  def __init__(self, **kwargs):
+    super(MockTask, self).__init__(**kwargs)
+  def execute(self):
+    pass
